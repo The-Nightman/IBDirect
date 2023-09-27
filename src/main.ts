3a6428cee66de372119a0d10d170829952b74fb8
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-28 00:11:28 +0100

    add vue router to main.ts with root view path

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Root from './views/Root.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Root },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
