e2ad880e3c5425e1edc1d0c41d1b7330af36d10a
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-28 22:32:27 +0100

    add routes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,37 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import './style.css'
-import App from './App.vue'
-import Root from './views/Root.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import Root from "./views/Root.vue";
+import { PatientLogin, StaffLogin, SwitchBoard } from "./components";
 
 const routes = [
-    { path: '/', component: Root },
+  {
+    path: "/",
+    component: Root,
+    children: [
+      {
+        path: "",
+        name: "IBDirect",
+        component: SwitchBoard,
+      },
+      {
+        path: "/Portal/Patient/Login",
+        name: "Patient portal login",
+        component: PatientLogin,
+      },
+      {
+        path: "/Portal/Staff/Login",
+        name: "Staff portal login",
+        component: StaffLogin,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
