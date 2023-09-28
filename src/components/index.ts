453ef7685c0f0b8cb1d5cd9ed101e404c70e441d
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-28 22:17:27 +0100

    update barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 import Header from "./Header.vue";
+import SwitchBoard from "./LoginSwitchBoard.vue";
+import PatientLogin from "./PatientLogin.vue";
+import StaffLogin from "./StaffLogin.vue";
 
-export { Header };
+
+export { Header, SwitchBoard, PatientLogin, StaffLogin };
