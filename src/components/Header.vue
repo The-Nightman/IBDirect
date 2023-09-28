a13b3d1e4d98a72308e905a3c8dca84eaaf2c837
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-28 16:09:43 +0100

    add header shell

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="flex max-w-screen justify-center h-24 bg-sky-600">
+        <h1 class="text-center text-5xl mb-3 self-end align-text-bottom text-white">IBDirect</h1>
+    </header>
+</template>
