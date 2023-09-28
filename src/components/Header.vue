834d8e0b9aca039fadd6d5008140277cdbb14695
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-28 17:00:10 +0100

    update style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,6 @@
 
 <template>
     <header class="flex max-w-screen justify-center h-24 bg-sky-600">
-        <h1 class="text-center text-5xl mb-3 self-end align-text-bottom text-white">IBDirect</h1>
+        <h1 class="text-5xl mb-3 self-end text-white">IBDirect</h1>
     </header>
 </template>
