419009ecef78caf626b099186847dc3465b6e428
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-29 22:59:25 +0100

    add Header.tsx migrated from Header.vue

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <>
+      <header className="flex max-w-screen justify-center h-24 bg-sky-600">
+        <h1 className="text-5xl mb-3 self-end text-white">IBDirect</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
