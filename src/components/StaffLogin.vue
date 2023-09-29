6ef9177e24cdd19ef5b812252f57080d4ac80cc0
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-29 23:03:40 +0100

    add inital form handling to StaffLogin.vue pre react migration

--- src/components/StaffLogin.vue
+++ src/components/StaffLogin.vue
@@ -1,4 +1,35 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Dialog from "primevue/dialog";
+import { ref } from "vue";
+const username = ref<string>("");
+const password = ref<string>("");
+const validName = ref<boolean>(true);
+let validPass = ref<boolean>(true);
+  const visible = ref<boolean>(false);
+
+function validateUsername() {
+  validName.value = false
+  // validName.value = /^223[A-Z]{2}[a-z]{0,5}\d{3}$/.test(username.value);
+  // visible.value = !validName.value
+  // console.log(visible.value)
+}
+
+function validatePass() {
+  if (password.value == "a") {
+    validPass.value = false
+  } else {
+    validPass.value = true
+  }
+}
+
+function handleLogin() {
+  const loginValues: { Name: string; Password: string } = {
+    Name: username.value,
+    Password: password.value,
+  };
+  console.log(loginValues);
+}
+</script>
 
 <template>
   <section class="w-72 mt-8 shadow-xl">
@@ -7,17 +38,26 @@
     >
       <h2 class="text-4xl self-center text-white">Sign In</h2>
     </div>
-    <div class="h-80 px-8 py-7 rounded-b bg-white">
-      <form class="flex flex-col h-full">
+    <div class="px-8 py-7 rounded-b bg-white">
+      <form class="flex flex-col h-full" @submit.prevent="handleLogin">
         <label class="text-sm" for="username"
           >Username
           <input
             type="text"
             id="username"
             name="Name"
-            value=""
+            v-model="username"
+            required
+            @blur="validateUsername"
+            placeholder="223JDoe123"
+            
             class="h-9 w-full px-1 rounded-t border-b-2 border-zinc-400 bg-zinc-300"
-        /></label>
+            :class="{ 'bg-red-300 border-red-500 ': !validName }"
+          />
+          <Dialog v-model:visible="visible" modal >
+            <p v-if="!validName" class="text-xs w-56 text-red-500">Invalid format: Use 223 followed by first initial up to 6 letters of surname and unique 3 num ID</p>
+        </Dialog>
+        </label>
         <br />
         <label class="text-sm mt-8" for="pass"
           >Password
@@ -25,8 +65,12 @@
             type="password"
             id="pass"
             name="Password"
-            value=""
+            v-model="password"
+            required
+            @blur="validatePass"
+            placeholder="Password"
             class="h-9 w-full px-1 rounded-t border-b-2 border-zinc-400 bg-zinc-300"
+            :class="{ 'bg-red-500': !validPass }"
         /></label>
         <input
           type="submit"
