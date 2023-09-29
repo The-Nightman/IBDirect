938179c982fb04c2a70e9813d0c03a32276d8698
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-29 22:58:57 +0100

    add index.ts barrel

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Header from "./Header.tsx";
+import LoginSwitchBoard from "./LoginSwitchBoard.tsx";
+import PatientLogin from "./PatientLogin.tsx";
+import StaffLogin from "./StaffLogin.tsx";
+
+
+export { Header, LoginSwitchBoard, PatientLogin, StaffLogin }
