f30d02af652a9553a7f49434e69b44bb274f261d
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-29 23:00:03 +0100

    add LoginSwitchBoard.tsx migrated from LoginSwitchBoard.vue

---
+++ src/components/LoginSwitchBoard.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const LoginSwitchBoard = () => {
+  return (
+    <>
+      <section className="w-72 mt-8 shadow-xl">
+        <div className="flex justify-center h-20 rounded-t bg-gradient-to-br from-sky-700 to-blue-400">
+          <h2 className="text-4xl self-center text-white">Sign In</h2>
+        </div>
+        <div className="h-80 px-8 py-14 rounded-b bg-white">
+          <div className="flex flex-col justify-between h-full">
+            <Link
+              to="/Portal/Patient/Login"
+              className="flex items-center justify-center w-full h-16 rounded text-2xl bg-zinc-300"
+              aria-label="Patient Login Button"
+            >
+              Patient Login
+            </Link>
+            <Link
+              to="/Portal/Staff/Login"
+              className="flex items-center justify-center w-full h-16 rounded text-2xl bg-zinc-300"
+              aria-label="Staff Login Button"
+            >
+              Staff Login
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LoginSwitchBoard;
