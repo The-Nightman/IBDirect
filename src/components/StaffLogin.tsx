4c3638bc3a3481d141b9c6489a1ee68314a00248
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-01 02:24:33 +0100

    add form handling and validation with accessibility tags

--- src/components/StaffLogin.tsx
+++ src/components/StaffLogin.tsx
@@ -1,4 +1,41 @@
+import { ChangeEvent, FocusEvent, FormEvent, useState } from "react";
+
 const StaffLogin = () => {
+  interface LoginForm {
+    Username: string;
+    Password: string;
+  }
+  const [valid, setValid] = useState<boolean>(true);
+  const [formData, setFormData] = useState<LoginForm>({
+    Username: "",
+    Password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleValidate = (e: FocusEvent<HTMLInputElement>): void => {
+    if (
+      !/^223[A-Z]{2}[a-z]{0,5}\d{3}$/.test(e.target.value) &&
+      e.target.value != ""
+    ) {
+      e.target.setCustomValidity("Invalid username format");
+      setValid(false);
+    } else {
+      e.target.setCustomValidity("");
+      setValid(true);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <section className="w-72 mt-8 shadow-xl">
@@ -6,42 +43,72 @@ const StaffLogin = () => {
           <h2 className="text-4xl self-center text-white">Sign In</h2>
         </div>
         <div className="px-8 py-7 rounded-b bg-white">
-          <form className="flex flex-col h-full">
-            <label className="text-sm" htmlFor="username">
-              Username
-              <input
-                type="text"
-                id="username"
-                name="Name"
-                v-model="username"
-                required
-                placeholder="223JDoe123"
-                className="h-9 w-full px-1 rounded-t border-b-2 border-zinc-400 bg-zinc-300"
-              />
-              <p className="text-xs w-56 text-red-500">
-                Invalid format: Use 223 followed by first initial up to 6
-                letters of surname and unique 3 num ID
-              </p>
-            </label>
-            <br />
-            <label className="text-sm mt-8" htmlFor="pass">
-              Password
-              <input
-                type="password"
-                id="pass"
-                name="Password"
-                v-model="password"
-                required
-                placeholder="Password"
-                className="h-9 w-full px-1 rounded-t border-b-2 border-zinc-400 bg-zinc-300"
-              />
-            </label>
-            <input
-              type="submit"
-              value="SIGN IN"
-              className="text-base underline decoration-2 mt-8 text-blue-600"
-            />
-            <p className="text-[0.5rem] mt-5">
+          <form onSubmit={handleSubmit}>
+            <fieldset className="flex flex-col h-full">
+              <legend className="sr-only">Staff login form</legend>
+              <div className="relative z-0 w-full group">
+                <input
+                  type="text"
+                  name="Username"
+                  id="Username"
+                  className={`block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 ${
+                    valid ? "border-gray-300" : "border-red-500"
+                  } appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
+                  placeholder=""
+                  required
+                  aria-required="true"
+                  aria-labelledby="UsernameLabel"
+                  aria-describedby="UsernameDesc"
+                  aria-invalid={!valid}
+                  value={formData.Username}
+                  onChange={handleChange}
+                  onBlur={handleValidate}
+                />
+                <label
+                  id="UsernameLabel"
+                  htmlFor="Username"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Username
+                </label>
+                <p id="UsernameDesc" className="text-[0.6rem] mt-1">
+                  Accepted format: 223 followed by capitalised first initial and
+                  surname up to the first 6 characters and unique 3 digit ID
+                  e.g. 223JDoe123
+                </p>
+              </div>
+              <br />
+              <div className="relative z-0 w-full mt-8  group">
+                <input
+                  type="Password"
+                  name="Password"
+                  id="Password"
+                  className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=""
+                  required
+                  aria-required="true"
+                  aria-labelledby="PasswordLabel"
+                  aria-describedby="PasswordLabel"
+                  value={formData.Password}
+                  onChange={handleChange}
+                />
+                <label
+                  id="PasswordLabel"
+                  htmlFor="Password"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Password
+                </label>
+              </div>
+              <button
+                className="self-center decoration-2 mt-6 p-2 w-fit text-base underline text-blue-600 hover:text-blue-400 disabled:text-zinc-500"
+                aria-label="Sign In Button"
+                disabled={!valid}
+              >
+                SIGN IN
+              </button>
+            </fieldset>
+            <p className="text-[0.5rem] mt-3">
               By proceeding, you confirm that you are the intended member of
               staff or administrator. Please be advised that it is an offence to
               access information not intended for you without the explicit
