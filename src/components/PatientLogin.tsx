d312a86de68787c3e37ce3febea17412ad4a219c
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-01 20:25:09 +0100

    add patient login logic and accessibility

--- src/components/PatientLogin.tsx
+++ src/components/PatientLogin.tsx
@@ -1,6 +1,65 @@
+import { ChangeEvent, FocusEvent, FormEvent, useState } from "react";
 import { DOBForm } from ".";
 
 const PatientLogin = () => {
+  interface LoginForm {
+    Name: string;
+    Password: string;
+  }
+  interface dobData {
+    day: string;
+    month: string;
+    year: string;
+  }
+  const [valid, setValid] = useState<boolean>(true);
+  const [dobData, setDobData] = useState<dobData>({
+    day: "",
+    month: "",
+    year: "",
+  });
+  const [formData, setFormData] = useState<LoginForm>({
+    Name: "",
+    Password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleValidate = (e: FocusEvent<HTMLInputElement>): void => {
+    if (!/^[A-Z]{2}[a-z]+\D/.test(e.target.value) && e.target.value != "") {
+      e.target.setCustomValidity("Invalid username format");
+      setValid(false);
+    } else {
+      e.target.setCustomValidity("");
+      setValid(true);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (Number(dobData.day) < 10) {
+      setDobData({
+        ...dobData,
+        day: `0${dobData.day}`,
+      });
+    }
+    if (Number(dobData.month) < 10) {
+      setDobData({
+        ...dobData,
+        month: `0${dobData.month}`,
+      });
+    }
+    const patientName = `${formData.Name}${dobData.day}${dobData.month}${dobData.year}`;
+    setFormData({
+      ...formData,
+      Name: patientName,
+    });
+  };
 
   return (
     <>
@@ -9,18 +68,27 @@ const PatientLogin = () => {
           <h2 className="text-4xl self-center text-white">Sign In</h2>
         </div>
         <div className="px-8 py-7 rounded-b bg-white">
-          <form>
+          <form onSubmit={handleSubmit}>
             <fieldset className="flex flex-col h-full">
               <legend className="sr-only">Patient login form</legend>
-              <DOBForm/>
+              <DOBForm setDobData={setDobData} />
               <div className="relative z-0 w-full mt-8 group">
                 <input
                   type="text"
                   name="Name"
                   id="Name"
-                  className={`block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
+                  className={`block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 ${
+                    valid ? "border-gray-300" : "border-red-500"
+                  } appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
                   placeholder=""
                   required
+                  aria-required="true"
+                  aria-labelledby="NameLabel"
+                  aria-describedby="NameDesc"
+                  aria-invalid={!valid}
+                  value={formData.Name}
+                  onChange={handleChange}
+                  onBlur={handleValidate}
                 />
                 <label
                   id="NameLabel"
@@ -43,6 +111,11 @@ const PatientLogin = () => {
                   className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=""
                   required
+                  aria-required="true"
+                  aria-labelledby="PasswordLabel"
+                  aria-describedby="PasswordLabel"
+                  value={formData.Password}
+                  onChange={handleChange}
                 />
                 <label
                   id="PasswordLabel"
@@ -52,7 +125,11 @@ const PatientLogin = () => {
                   Password
                 </label>
               </div>
-              <button className="self-center decoration-2 mt-6 p-2 w-fit text-base underline text-blue-600 hover:text-blue-400 disabled:text-zinc-500">
+              <button
+                className="self-center decoration-2 mt-6 p-2 w-fit text-base underline text-blue-600 hover:text-blue-400 disabled:text-zinc-500"
+                aria-label="Sign In Button"
+                disabled={!valid}
+              >
                 SIGN IN
               </button>
             </fieldset>
