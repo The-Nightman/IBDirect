9d46967cec5fbbb027f9ee558f4777bccf65963b
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-02 18:54:05 +0100

    Revert "add stafflogin request"
    This reverts commit 379235a765510f165a44d8d86e16bc35c0fa83a1.

--- src/api/staffLogin.js
+++
@@ -1,9 +0,0 @@
-import { IBDirectAPI } from "./api";
-
-export const patientLoginAPI = (formData) => {
-    return IBDirectAPI
-        .post("/Account/staff/patient", formData)
-        .then((res) => {
-            return res
-        })
-}
