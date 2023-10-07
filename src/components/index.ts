b0d228cf87ab30d636fca668c411069e0dc2404a
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-07 20:01:35 +0100

    update imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 import Header from "./Header.tsx";
-import LoginSwitchBoard from "./LoginSwitchBoard.tsx";
-import PatientLogin from "./PatientLogin.tsx";
-import StaffLogin from "./StaffLogin.tsx";
-import DOBForm from "./DOBform.tsx";
+import LoginSwitchBoard from "./Login/LoginSwitchBoard.tsx";
+import PatientLogin from "./Login/PatientLogin.tsx";
+import StaffLogin from "./Login/StaffLogin.tsx";
+import DOBForm from "./Forms/DOBform.tsx";
 
 
 export { Header, LoginSwitchBoard, PatientLogin, StaffLogin, DOBForm }
