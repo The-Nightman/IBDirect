3819232b39b0c7fb907199f5d6187016f86ae50e
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-11 15:52:28 +0100

    add privateroutes to barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import LoginSwitchBoard from "./Login/LoginSwitchBoard.tsx";
 import PatientLogin from "./Login/PatientLogin.tsx";
 import StaffLogin from "./Login/StaffLogin.tsx";
 import DOBForm from "./Forms/DOBform.tsx";
+import PrivateRoutes from "./utility/PrivateRoutes.tsx";
 
 
-export { Header, LoginSwitchBoard, PatientLogin, StaffLogin, DOBForm }
+export { Header, LoginSwitchBoard, PatientLogin, StaffLogin, DOBForm, PrivateRoutes }
