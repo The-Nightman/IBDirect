f64d04f406a83afdd6a726fd7eedc8ec6db6485b
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-13 22:18:51 +0100

    add staff dashboard page and onmount calls

--- src/pages/StaffDash.tsx
+++ src/pages/StaffDash.tsx
@@ -1,7 +1,46 @@
-
+import { useEffect, useState } from "react";
+import { userStaffDetails } from "../api/getStaffUserDetails";
+import { useAuth } from "../context/AuthContext";
+import { StaffDashboard } from "../components";
 
 export default function StaffDash() {
+  interface userData {
+    name: string;
+    practice: string;
+    role: string;
+    speciality: string;
+    staffId: number | null;
+  }
+
+  const [userData, setUserData] = useState<userData>({
+    name: "",
+    practice: "",
+    role: "",
+    speciality: "",
+    staffId: null,
+  });
+  const { user } = useAuth();
+
+  useEffect(() => {
+    userStaffDetails(user.userID)
+      .then((res) => {
+        setUserData(res.data);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  }, []);
+
   return (
-    <div>StaffDash</div>
-  )
+    <>
+      <div className="flex justify-center">
+        <section className="w-72 mt-8 shadow-xl">
+          <div className="flex justify-center h-20 rounded-t bg-gradient-to-br from-sky-700 to-blue-400">
+            <h2 className="text-4xl self-center text-white">Sign In</h2>
+          </div>
+          <StaffDashboard />
+        </section>
+      </div>
+    </>
+  );
 }
