e588f1d2f8a593b4be437801bc083928dd6d0f78
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-03-25 22:29:36 +0000

    breakaway prescription interface from patientDetails interface and update to include new data and optional properties required for create and update operations

---
+++ src/interfaces/Prescription.ts
@@ -0,0 +1,13 @@
+import { StaffDetails } from "./StaffDetails";
+
+export interface Prescription {
+  id: number;
+  scriptName: string;
+  scriptStartDate: string;
+  scriptDose: string;
+  scriptInterval: string;
+  scriptNotes: string;
+  scriptRepeat: boolean;
+  prescribingStaffId?: number;
+  prescribingStaff?: StaffDetails;
+}
