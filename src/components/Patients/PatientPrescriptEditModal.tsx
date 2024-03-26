0e9c101d489b8704dad04873004d0585f88f89ae
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-03-26 13:45:06 +0000

    add edit butons and states required for edit functionality

--- src/components/Patients/PatientPrescriptEditModal.tsx
+++ src/components/Patients/PatientPrescriptEditModal.tsx
@@ -16,7 +16,9 @@ const PatientPrescriptEditModal = ({
 }: PatientPrescriptEditModalProps) => {
   const [prescriptionData, setPrescriptionData] =
     useState<Prescription>(prescription);
+  const [editPrescription, setEditPrescription] = useState<boolean>(false);
   const [notes, setNotes] = useState<string>("");
+  const [editNotes, setEditNotes] = useState<boolean>(false);
   const notesAreaRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
@@ -40,9 +42,30 @@ const PatientPrescriptEditModal = ({
     };
   }, [notes]);
 
+  const handleEditNotes = () => {
+    if (editNotes) {
+      setNotes(prescription.scriptNotes);
+    }
+    setEditNotes(!editNotes);
+  };
+
+  const handleEditPrescription = () => {
+    if (editPrescription) {
+      setPrescriptionData(prescription);
+    }
+    setEditPrescription(!editPrescription);
+  };
+
+  const handleSaveNotes = () => {
+    setPrescriptionData({ ...prescriptionData, scriptNotes: notes });
+    setEditNotes(false);
+  };
+
   const closeDialog = () => {
     setPrescriptionData(prescription);
     setNotes(prescription.scriptNotes);
+    setEditPrescription(false);
+    setEditNotes(false);
     setEditModalState(false);
   };
 
@@ -56,7 +79,17 @@ const PatientPrescriptEditModal = ({
         prescription.scriptName
       } - ${parseIsoToDateOnly(prescription.scriptStartDate)}`}
     >
-      <div className="m-4">
+      <div className="relative m-4">
+        <button
+          className={`absolute right-0 rounded-sm px-1 ${
+            !editPrescription
+              ? "bg-zinc-400 hover:bg-zinc-700 active:bg-zinc-500"
+              : "bg-red-400 hover:bg-red-700 active:bg-red-500"
+          } hover:text-white`}
+          onClick={handleEditPrescription}
+        >
+          {editPrescription ? "Cancel Edit Prescription" : "Edit Prescription"}
+        </button>
         <h3 className="flex flex-wrap justify-between mb-4">
           <strong>{prescriptionData.scriptName}</strong>
           <strong>{prescriptionData.scriptStartDate}</strong>
@@ -76,6 +109,26 @@ const PatientPrescriptEditModal = ({
           <p>{prescription.prescribingStaff!.practice}</p>
         </div>
         <div className="my-4">
+          <div className="flex justify-between mb-1">
+            <button
+              className={`rounded-sm px-1 ${
+                !editNotes
+                  ? "bg-zinc-400 hover:bg-zinc-700 active:bg-zinc-500"
+                  : "bg-red-400 hover:bg-red-700 active:bg-red-500"
+              } hover:text-white`}
+              onClick={handleEditNotes}
+            >
+              {editNotes ? "Cancel Edit" : "Edit Notes"}
+            </button>
+            {editNotes && (
+              <button
+                className="rounded-sm px-1 bg-blue-400 hover:bg-sky-700 active:bg-sky-500 hover:text-white"
+                onClick={handleSaveNotes}
+              >
+                Save Notes
+              </button>
+            )}
+          </div>
           <textarea
             disabled={true}
             aria-label="Prescription Notes"
@@ -84,19 +137,24 @@ const PatientPrescriptEditModal = ({
             ref={notesAreaRef}
           />
         </div>
-        <div className="flex gap-16 mt-2">
-          <button
-            className="rounded-sm py-2 px-4 bg-zinc-400 hover:bg-zinc-700 active:bg-zinc-500 hover:text-white"
-            onClick={closeDialog}
-          >
-            close
-          </button>
-          <button
-            className="rounded-sm py-2 px-4 bg-blue-400 hover:bg-sky-700 active:bg-sky-500 hover:text-white"
-            onClick={savePrescription}
-          >
-            Save Prescription
-          </button>
+        <div className="flex flex-col items-center mt-8">
+          <strong>
+            Please save prescription before closing to prevent loss of updates.
+          </strong>
+          <div className="flex gap-16 mt-2">
+            <button
+              className="rounded-sm py-2 px-4 bg-zinc-400 hover:bg-zinc-700 active:bg-zinc-500 hover:text-white"
+              onClick={closeDialog}
+            >
+              close
+            </button>
+            <button
+              className="rounded-sm py-2 px-4 bg-blue-400 hover:bg-sky-700 active:bg-sky-500 hover:text-white"
+              onClick={savePrescription}
+            >
+              Save Prescription
+            </button>
+          </div>
         </div>
       </div>
     </Modal>
