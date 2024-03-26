bd14db7555fc7e8b5374bbe95e1b186b4882f78c
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-03-26 18:09:12 +0000

    add new userDetailsContext to store important user details for sensitive user explicit operations such as prescriptions

---
+++ src/context/userDetailsContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { StaffDetails } from "../interfaces/StaffDetails";
+import { useAuth } from "./AuthContext";
+import { userStaffDetails } from "../api/getStaffUserDetails";
+
+interface UserDetailsContextInterface {
+  userDetails: StaffDetails | null;
+}
+
+interface UserDetailsProviderProps {
+  children: React.ReactNode;
+}
+
+const UserDetailsContext = createContext<
+  UserDetailsContextInterface | undefined
+>(undefined);
+
+export const UserDetailsProvider = ({ children }: UserDetailsProviderProps) => {
+  const { user } = useAuth();
+  const [userDetails, setUserDetails] = useState<StaffDetails | null>(null);
+
+  useEffect(() => {
+    if (user.userID) {
+      userStaffDetails(user.userID)
+        .then((res) => {
+          setUserDetails(res.data);
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+    }
+  }, [user.userID]);
+
+  return (
+    <UserDetailsContext.Provider value={{ userDetails }}>
+      {children}
+    </UserDetailsContext.Provider>
+  );
+};
+
+export const useUserDetails = () => {
+  return useContext(UserDetailsContext) as UserDetailsContextInterface;
+};
