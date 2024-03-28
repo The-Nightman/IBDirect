d3d8ac10e31fccfbdab4ee4b476a457355ed3e44
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-03-28 16:49:20 +0000

    remove missed console.log from testing

--- src/api/cancelPrescription.js
+++ src/api/cancelPrescription.js
@@ -1,7 +1,6 @@
 import { IBDirectAPI } from "./api";
 
 export const cancelPrescription = (id) => {
-    console.log(id)
   const jwt = sessionStorage.getItem("jwt");
   return IBDirectAPI.put(`/Patients/cancelPrescription/${id}`, {}, {
     headers: {
