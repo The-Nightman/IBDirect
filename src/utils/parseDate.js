c6eb43197bb611d78a1de47732d90794ecb9bf5d
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-03-28 20:28:45 +0000

    extend parseDate function to produce shorthand year dates

--- src/utils/parseDate.js
+++ src/utils/parseDate.js
@@ -1,3 +1,9 @@
-export const parseDate = (date) => {
-  return date.split("-").reverse().join("/");
+export const parseDate = (date, short = false) => {
+  const formattedDate = date.split("-").reverse();
+  if (short === true) {
+    formattedDate[formattedDate.length - 1] =
+      formattedDate[formattedDate.length - 1].slice(-2);
+  }
+
+  return formattedDate.join("/");
 };
