d52bdcf7ed03a5b99f2c6d17557ae0e760e8bc90
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-01 00:35:00 +0100

    refactor getStaffPatientsList api call function url to match updated controller structure in backend

--- src/api/getStaffPatientsList.js
+++ src/api/getStaffPatientsList.js
@@ -4,7 +4,7 @@ import { IBDirectAPI } from "./api";
 export const staffPatientsList = (id) => {
   const jwt = sessionStorage.getItem("jwt");
   const { role } = jwtDecode(jwt)
-  return IBDirectAPI.get(`/Patients/mypatients/${role}/${id}`, {
+  return IBDirectAPI.get(`/Staff/mypatients/${role}/${id}`, {
     headers: {
       Authorization: `Bearer ${jwt}`,
     },
