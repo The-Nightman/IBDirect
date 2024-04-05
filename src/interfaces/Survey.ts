483398c80873f8674bdceb213701f8ddc86b6d8c
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-05 20:10:14 +0100

    breakaway Survey interface from PatientDetails interface and refactor to match updated properties and nullables

---
+++ src/interfaces/Survey.ts
@@ -0,0 +1,28 @@
+export interface Survey {
+  id?: number;
+  date: string;
+  q1: number | null;
+  q2: number | null;
+  q3: number | null;
+  q4: number | null;
+  q4a: boolean | null;
+  q5: number | null;
+  q6: number | null;
+  q7: number | null;
+  q8: number | null;
+  q9: number | null;
+  q10: number | null;
+  q11: number | null;
+  q12: number | null;
+  contScore: number | null;
+  q13: number | null;
+  q14: number | null;
+  q15: number | null;
+  q16: number | null;
+  q16a: string;
+  q17: number | null;
+  q17a: string;
+  q18: number | null;
+  q19: number | null;
+  completed: boolean;
+}
