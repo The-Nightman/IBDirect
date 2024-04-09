06e88343a41b8877458cd5ce3a6b2005cb2a8d81
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-09 20:08:23 +0100

    add ux, error handling and saving handling functions to PatientSurveyPatientEditModal as well as pass down function for optimistic render

--- src/components/Patients/PatientSurveyPatientEditModal.tsx
+++ src/components/Patients/PatientSurveyPatientEditModal.tsx
@@ -2,18 +2,71 @@ import { Modal } from "flowbite-react";
 import { Survey } from "../../interfaces/Survey";
 import { parseIsoToDateOnly } from "../../utils/parseIsoToDateOnly";
 import IBDSurveyForm from "../Forms/IBDSurveyForm";
+import { useState } from "react";
+import { updateSurvey } from "../../api/updateSurvey";
+import Toast from "../UX/Toast";
+import { ErrorState } from "../../interfaces/ErrorState";
 
 interface PatientSurveyPatientEditModalProps {
   survey: Survey;
   editModalState: boolean;
   setEditModalState: (state: boolean) => void;
+  removeCompletedSurvey: (id: number) => void;
 }
 
 const PatientSurveyPatientEditModal = ({
   survey,
   editModalState,
   setEditModalState,
+  removeCompletedSurvey,
 }: PatientSurveyPatientEditModalProps) => {
+  const [formData, setFormData] = useState<Survey>(survey);
+  const [toastState, setToastState] = useState<ErrorState>({
+    state: false,
+    message: "",
+    color: "",
+  });
+
+  const saveSurvey = () => {
+    if (Object.values(formData).some((value) => value === null)) {
+      setToastState({
+        state: true,
+        message: "Please fill out all fields",
+        color: "failure",
+      });
+      return null;
+    }
+    updateSurvey(survey.id, { ...formData, completed: true })
+      .then((_res) => {
+        removeCompletedSurvey(survey.id!);
+        setToastState({
+          state: true,
+          message: "Survey successfully updated",
+          color: "success",
+        });
+      })
+      .catch((err) => {
+        if (err.response === undefined) {
+          setToastState({ ...toastState, state: true });
+        } else {
+          setToastState({
+            state: true,
+            message: err.response.data,
+            color: "failure",
+          });
+        }
+      });
+  };
+
+  const closeToastState = () => {
+    setToastState({ state: false, message: "", color: "failure" });
+  };
+
+  const closeDialog = () => {
+    setFormData(survey);
+    setEditModalState(false);
+  };
+
   return (
     <Modal
       show={editModalState}
@@ -27,19 +80,38 @@ const PatientSurveyPatientEditModal = ({
         survey.date
       )}`}
     >
-      <div className="m-4 overflow-y-scroll">
+      <div className="m-4 px-2 overflow-y-scroll">
         <h3 className="flex flex-wrap justify-between mb-4 text-lg font-semibold">
           <p>IBD Survey</p>
           <time>{parseIsoToDateOnly(survey.date)}</time>
         </h3>
-        <IBDSurveyForm survey={survey} patientUserEdit={true} />
-        <div className="flex flex-col items-center">
-          <button
-            className="rounded-sm py-2 px-4 bg-zinc-400 hover:bg-zinc-700 active:bg-zinc-500 hover:text-white"
-            onClick={() => setEditModalState(false)}
-          >
-            close
-          </button>
+        <IBDSurveyForm
+          survey={survey}
+          patientUserEdit={true}
+          setParentFormData={setFormData}
+        />
+        <div className="flex flex-col items-center mt-8">
+          {toastState.state && (
+            <Toast
+              color={toastState.color || "failure"}
+              message={toastState.message}
+              handleErrorState={closeToastState}
+            />
+          )}
+          <div className="flex gap-16 mt-2">
+            <button
+              className="rounded-sm py-2 px-4 bg-zinc-400 hover:bg-zinc-700 active:bg-zinc-500 hover:text-white"
+              onClick={closeDialog}
+            >
+              Close
+            </button>
+            <button
+              className="rounded-sm py-2 px-4 bg-blue-400 hover:bg-sky-700 active:bg-sky-500 hover:text-white"
+              onClick={saveSurvey}
+            >
+              Save Survey
+            </button>
+          </div>
         </div>
       </div>
     </Modal>
