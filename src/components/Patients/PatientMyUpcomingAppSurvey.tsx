07846ae089e0bdf2c6cc46270c97de7c2c68488a
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-09 22:39:34 +0100

    add loading spinner to PatientMyUpcomingAppSurvey

--- src/components/Patients/PatientMyUpcomingAppSurvey.tsx
+++ src/components/Patients/PatientMyUpcomingAppSurvey.tsx
@@ -7,6 +7,7 @@ import { ErrorState } from "../../interfaces/ErrorState";
 import Toast from "../UX/Toast";
 import PatientAppointmentCard from "./PatientAppointmentCard";
 import PatientSurveyCard from "./PatientSurveyCard";
+import SpinnerStatus from "../UX/Spinner";
 
 interface UpcomingData {
   appointments: Appointment[];
@@ -14,6 +15,7 @@ interface UpcomingData {
 }
 
 const PatientMyUpcomingAppSurvey = () => {
+  const [loading, setLoading] = useState<boolean>(true);
   const [upcomingData, setUpcomingData] = useState<UpcomingData>({
     appointments: [],
     surveys: [],
@@ -29,6 +31,7 @@ const PatientMyUpcomingAppSurvey = () => {
     getPatientMyUpcoming(user.userID)
       .then((res) => {
         setUpcomingData(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         if (err.response === undefined) {
@@ -39,6 +42,7 @@ const PatientMyUpcomingAppSurvey = () => {
             message: err.response.data,
             color: "failure",
           });
+          setLoading(false);
         }
       });
   }, [user.userID]);
@@ -49,73 +53,76 @@ const PatientMyUpcomingAppSurvey = () => {
 
   const removeCompletedSurvey = (id: number) => {
     setUpcomingData({
-        ...upcomingData,
-        surveys: upcomingData.surveys.filter(
-          (survey) => survey.id !== id
-        ),
-      });
+      ...upcomingData,
+      surveys: upcomingData.surveys.filter((survey) => survey.id !== id),
+    });
   };
 
   return (
-    <section className="flex flex-col lg:flex-row gap-8">
-      {toastState.state && (
-        <Toast
-          color={toastState.color || "failure"}
-          message={toastState.message}
-          handleErrorState={closeToastState}
-        />
-      )}
-      <section className="lg:w-2/4">
-        <h3 className="border-b border-slate-400 mb-4">
-          My Upcoming Appointments
-        </h3>
-        <ol className="border-x border-t border-slate-500">
-          {upcomingData.appointments.length > 0 ? (
-            upcomingData.appointments.map((appointment, index) => {
-              return (
-                <li key={index}>
-                  <PatientAppointmentCard
-                    appointment={appointment}
-                    index={index}
-                  />
-                </li>
-              );
-            })
-          ) : (
-            <li>
-              <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
-                <p>No upcoming appointments to display</p>
-              </div>
-            </li>
-          )}
-        </ol>
+    <>
+      {loading && <SpinnerStatus />}
+      <section className="flex flex-col lg:flex-row gap-8">
+        {toastState.state && (
+          <Toast
+            color={toastState.color || "failure"}
+            message={toastState.message}
+            handleErrorState={closeToastState}
+          />
+        )}
+        <section className="lg:w-2/4">
+          <h3 className="border-b border-slate-400 mb-4">
+            My Upcoming Appointments
+          </h3>
+          <ol className="border-x border-t border-slate-500">
+            {upcomingData.appointments.length > 0 ? (
+              upcomingData.appointments.map((appointment, index) => {
+                return (
+                  <li key={index}>
+                    <PatientAppointmentCard
+                      appointment={appointment}
+                      index={index}
+                    />
+                  </li>
+                );
+              })
+            ) : (
+              <li>
+                <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
+                  <p>No upcoming appointments to display</p>
+                </div>
+              </li>
+            )}
+          </ol>
+        </section>
+        <section className="lg:w-2/4">
+          <h3 className="border-b border-slate-400 mb-4">
+            My Upcoming Surveys
+          </h3>
+          <ol className="border-x border-t border-slate-500">
+            {upcomingData.surveys.length > 0 ? (
+              upcomingData.surveys.map((survey, index) => {
+                return (
+                  <li key={index}>
+                    <PatientSurveyCard
+                      survey={survey}
+                      patientUserEdit={true}
+                      index={index}
+                      removeSurvey={removeCompletedSurvey}
+                    />
+                  </li>
+                );
+              })
+            ) : (
+              <li>
+                <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
+                  <p>No upcoming surveys to display</p>
+                </div>
+              </li>
+            )}
+          </ol>
+        </section>
       </section>
-      <section className="lg:w-2/4">
-        <h3 className="border-b border-slate-400 mb-4">My Upcoming Surveys</h3>
-        <ol className="border-x border-t border-slate-500">
-          {upcomingData.surveys.length > 0 ? (
-            upcomingData.surveys.map((survey, index) => {
-              return (
-                <li key={index}>
-                  <PatientSurveyCard
-                    survey={survey}
-                    patientUserEdit={true}
-                    index={index}
-                    removeSurvey={removeCompletedSurvey}
-                  />
-                </li>
-              );
-            })
-          ) : (
-            <li>
-              <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
-                <p>No upcoming surveys to display</p>
-              </div>
-            </li>
-          )}
-        </ol>
-      </section>
-    </section>
+    </>
   );
 };
 
