9aeffbfa31700d7804d9db1348922d013b96eb76
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-11 00:38:50 +0100

    swap article and section tags for semantics in StaffMyPersonalDetails

--- src/components/Staff/StaffMyPersonalDetails.tsx
+++ src/components/Staff/StaffMyPersonalDetails.tsx
@@ -43,7 +43,7 @@ const StaffMyPersonalDetails = () => {
   return (
     <>
       {loading && <SpinnerStatus />}
-      <section className="flex flex-col p-6">
+      <article className="flex flex-col p-6">
         <h2 className="text-3xl mb-4 pb-2 border-b border-slate-300">My Details</h2>
         {toastState.state && (
           <Toast
@@ -52,7 +52,7 @@ const StaffMyPersonalDetails = () => {
             handleErrorState={closeToastState}
           />
         )}
-        <article className="mt-10">
+        <section className="mt-10">
           <h3 className="text-2xl mb-4">{myDetails?.name}</h3>
           <div className="w-72 flex flex-col flex-wrap gap-6">
             <p>
@@ -68,8 +68,8 @@ const StaffMyPersonalDetails = () => {
               <span className="mt-2 ml-4">{myDetails?.practice}</span>
             </p>
           </div>
-        </article>
-      </section>
+        </section>
+      </article>
     </>
   );
 };
