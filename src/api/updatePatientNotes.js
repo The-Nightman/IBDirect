c71b34bf16cb8edb77d4de8c77a7342751574654
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-14 17:41:08 +0100

    switch updatePatientNotes to patch method

--- src/api/updatePatientNotes.js
+++ src/api/updatePatientNotes.js
@@ -2,7 +2,7 @@ import { IBDirectAPI } from "./api";
 
 export const updatePatientNotes = (id, updatedNotes) => {
   const jwt = sessionStorage.getItem("jwt");
-  return IBDirectAPI.put(`/Patients/${id}/updateNotes`, { notes: updatedNotes }, {
+  return IBDirectAPI.patch(`/Patients/${id}/updateNotes`, { notes: updatedNotes }, {
     headers: {
       Authorization: `Bearer ${jwt}`,
     },
