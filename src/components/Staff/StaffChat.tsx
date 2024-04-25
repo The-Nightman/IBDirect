c3a87ae1bf29711d06694ea11710f29e102c0517
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-25 15:55:19 +0100

    add chat window to StaffChat and appropriate props and handling functions

--- src/components/Staff/StaffChat.tsx
+++ src/components/Staff/StaffChat.tsx
@@ -10,8 +10,9 @@ import { StaffDetails } from "../../interfaces/StaffDetails";
 import { getMyChatInbox } from "../../api/getMyChatInbox";
 import { parseIsoToDateTime } from "../../utils/parseIsoToDateTime";
 import { ChatInboxItem } from "../../interfaces/ChatInboxItem";
+import ChatWindow from "../Chat/ChatWindow";
 
-interface StaffChatUserDetails {
+interface ChatUserDetails {
   userId: number;
   name: string;
   role: string;
@@ -19,7 +20,7 @@ interface StaffChatUserDetails {
 
 interface StaffChatProps {
   setChatState: React.Dispatch<React.SetStateAction<boolean>>;
-  userDetails: StaffChatUserDetails;
+  userDetails: ChatUserDetails;
 }
 
 const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
@@ -27,6 +28,12 @@ const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
   const [myInbox, setMyInbox] = useState<ChatInboxItem[]>([]);
   const [staffCollapsed, setStaffCollapsed] = useState<boolean>(true);
   const [inboxCollapsed, setInboxCollapsed] = useState<boolean>(false);
+  const [chatWindowState, setChatWindowState] = useState<boolean>(false);
+  const [selectedUserData, setSelectedUserData] = useState<ChatUserDetails>({
+    userId: 0,
+    name: "",
+    role: "",
+  });
 
   useEffect(() => {
     getStaffMyColleagues(userDetails.userId)
@@ -50,6 +57,11 @@ const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
     return message.length > 30 ? `${message.substring(0, 30)}...` : message;
   };
 
+  const handleOpenChat = (id: number, role: string, name: string) => {
+    setSelectedUserData({ userId: id, role, name });
+    setChatWindowState(true);
+  };
+
   return (
     <div className="flex flex-col fixed bottom-4 right-4 z-10 h-[30rem] w-80 rounded border border-slate-400 bg-white">
       <div className="flex justify-between p-1 rounded-t bg-gradient-to-br from-sky-700 to-blue-400 text-white">
@@ -58,104 +70,126 @@ const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
           <CloseOutlined />
         </button>
       </div>
-      <div className="flex-grow overflow-y-auto">
-        <section>
-          <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
-            <h4>My Inbox</h4>
-            <button
-              aria-label={inboxCollapsed ? "Show My Inbox" : "Hide My Inbox"}
-              onClick={() => setInboxCollapsed(!inboxCollapsed)}
-            >
-              {inboxCollapsed ? (
-                <>
-                  Show <ExpandMoreOutlined />
-                </>
-              ) : (
-                <>
-                  Hide <ExpandLessOutlined />
-                </>
-              )}
-            </button>
-          </div>
-          {!inboxCollapsed && (
-            <ol>
-              {myInbox.length ? (
-                myInbox.map((inbox) => (
+      {chatWindowState ? (
+        <ChatWindow
+          chatUsers={{
+            currentId: userDetails.userId,
+            currentName: userDetails.name,
+            currentRole: userDetails.role,
+            recipientId: selectedUserData.userId,
+            recipientName: selectedUserData.name,
+            recipientRole: selectedUserData.role,
+          }}
+          setChatWindowState={setChatWindowState}
+          setSelectedUserData={setSelectedUserData}
+        />
+      ) : (
+        <div className="flex-grow overflow-y-auto">
+          <section aria-live="polite">
+            <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
+              <h4>My Inbox</h4>
+              <button
+                aria-label={inboxCollapsed ? "Show My Inbox" : "Hide My Inbox"}
+                onClick={() => setInboxCollapsed(!inboxCollapsed)}
+              >
+                {inboxCollapsed ? (
+                  <>
+                    Show <ExpandMoreOutlined />
+                  </>
+                ) : (
+                  <>
+                    Hide <ExpandLessOutlined />
+                  </>
+                )}
+              </button>
+            </div>
+            {!inboxCollapsed && (
+              <ol>
+                {myInbox.length ? (
+                  myInbox.map((inbox) => (
+                    <li
+                      className="p-1 bg-slate-100 hover:bg-slate-200 active:bg-slate-400 border-b border-slate-300"
+                      key={inbox.senderId}
+                    >
+                      <button
+                        className="flex flex-col w-full text-justify"
+                        onClick={() =>
+                          handleOpenChat(
+                            inbox.senderId,
+                            inbox.senderName,
+                            inbox.senderRole
+                          )
+                        }
+                      >
+                        <div className="flex justify-between">
+                          <p>
+                            {inbox.senderName} - {inbox.senderRole}
+                          </p>
+                          <span
+                            className="w-6 rounded-full bg-red-400 text-center"
+                            aria-label="unread messages"
+                          >
+                            {inbox.unreadMessages}
+                          </span>
+                        </div>
+                        <div>
+                          <p>{truncatePreview(inbox.content)}</p>
+                          <time>{parseIsoToDateTime(inbox.mostRecent)}</time>
+                        </div>
+                      </button>
+                    </li>
+                  ))
+                ) : (
+                  <li className="p-1 bg-slate-100 border-b border-slate-300">
+                    <p>No new messages</p>
+                  </li>
+                )}
+              </ol>
+            )}
+          </section>
+          <section>
+            <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
+              <h4>My Staff</h4>
+              <button
+                aria-label={
+                  staffCollapsed ? "Show Staff Chats" : "Hide Staff Chats"
+                }
+                onClick={() => setStaffCollapsed(!staffCollapsed)}
+              >
+                {staffCollapsed ? (
+                  <>
+                    Show <ExpandMoreOutlined />
+                  </>
+                ) : (
+                  <>
+                    Hide <ExpandLessOutlined />
+                  </>
+                )}
+              </button>
+            </div>
+            {!staffCollapsed && (
+              <ul>
+                {staffChats.map((staff) => (
                   <li
-                    className="p-1 bg-slate-100 hover:bg-slate-200 active:bg-slate-400 border-b border-slate-300"
-                    key={inbox.senderId}
+                    className="flex justify-between p-1 bg-slate-100 border-b border-slate-300"
+                    key={staff.staffId}
                   >
-                    <button className="flex flex-col w-full text-justify">
-                      <div className="flex justify-between">
-                        <p>
-                          {inbox.senderName} - {inbox.senderRole}
-                        </p>
-                        <span
-                          className="w-6 rounded-full bg-red-400 text-center"
-                          aria-label="unread messages"
-                        >
-                          {inbox.unreadMessages}
-                        </span>
-                      </div>
-                      <div>
-                        <p>
-                          {truncatePreview("inbox.content test test test test")}
-                        </p>
-                        <time>{parseIsoToDateTime(inbox.mostRecent)}</time>
-                      </div>
+                    <p>
+                      {staff.name} - {staff.role}
+                    </p>
+                    <button
+                      className="h-7 w-7 rounded-full bg-blue-300 hover:bg-blue-400 active:bg-blue-600 active:text-white text-center"
+                      aria-label={`open staff chat ${staff.name}`}
+                    >
+                      <ChatBubbleOutline fontSize="small" />
                     </button>
                   </li>
-                ))
-              ) : (
-                <li className="p-1 bg-slate-100 border-b border-slate-300">
-                  <p>No new messages</p>
-                </li>
-              )}
-            </ol>
-          )}
-        </section>
-        <section>
-          <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
-            <h4>My Staff</h4>
-            <button
-              aria-label={
-                staffCollapsed ? "Show Staff Chats" : "Hide Staff Chats"
-              }
-              onClick={() => setStaffCollapsed(!staffCollapsed)}
-            >
-              {staffCollapsed ? (
-                <>
-                  Show <ExpandMoreOutlined />
-                </>
-              ) : (
-                <>
-                  Hide <ExpandLessOutlined />
-                </>
-              )}
-            </button>
-          </div>
-          {!staffCollapsed && (
-            <ul>
-              {staffChats.map((staff) => (
-                <li
-                  className="flex justify-between p-1 bg-slate-100 border-b border-slate-300"
-                  key={staff.staffId}
-                >
-                  <p>
-                    {staff.name} - {staff.role}
-                  </p>
-                  <button
-                    className="h-7 w-7 rounded-full bg-blue-300 hover:bg-blue-400 active:bg-blue-600 active:text-white text-center"
-                    aria-label={`open staff chat ${staff.name}`}
-                  >
-                    <ChatBubbleOutline fontSize="small" />
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-        </section>
-      </div>
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      )}
     </div>
   );
 };
