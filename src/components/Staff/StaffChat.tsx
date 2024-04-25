e1fa7f7c0714fc5fb0b13dd608f90caf3b9e53bb
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-25 17:24:58 +0100

    refactor StaffChat to show recent chats alongside unread chats and new inbox data shape

--- src/components/Staff/StaffChat.tsx
+++ src/components/Staff/StaffChat.tsx
@@ -9,13 +9,19 @@ import { getStaffMyColleagues } from "../../api/getStaffMyColleagues";
 import { StaffDetails } from "../../interfaces/StaffDetails";
 import { getMyChatInbox } from "../../api/getMyChatInbox";
 import { parseIsoToDateTime } from "../../utils/parseIsoToDateTime";
-import { ChatInboxItem } from "../../interfaces/ChatInboxItem";
+import { ChatInbox } from "../../interfaces/ChatInbox";
 import ChatWindow from "../Chat/ChatWindow";
 
 interface ChatUserDetails {
   userId: number;
   name: string;
   role: string;
+}
+
+interface InboxTabsCollapse {
+  inboxCollapsed: boolean;
+  unreadCollapsed: boolean;
+  recentCollapsed: boolean;
 }
 
 interface StaffChatProps {
@@ -25,9 +31,17 @@ interface StaffChatProps {
 
 const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
   const [staffChats, setStaffChats] = useState<StaffDetails[]>([]);
-  const [myInbox, setMyInbox] = useState<ChatInboxItem[]>([]);
+  const [myInbox, setMyInbox] = useState<ChatInbox>({
+    unreadChats: [],
+    recentChats: [],
+  });
   const [staffCollapsed, setStaffCollapsed] = useState<boolean>(true);
-  const [inboxCollapsed, setInboxCollapsed] = useState<boolean>(false);
+  const [inboxTabsCollapsed, setInboxTabsCollapsed] =
+    useState<InboxTabsCollapse>({
+      inboxCollapsed: false,
+      unreadCollapsed: true,
+      recentCollapsed: true,
+    });
   const [chatWindowState, setChatWindowState] = useState<boolean>(false);
   const [selectedUserData, setSelectedUserData] = useState<ChatUserDetails>({
     userId: 0,
@@ -86,13 +100,37 @@ const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
       ) : (
         <div className="flex-grow overflow-y-auto">
           <section aria-live="polite">
-            <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
-              <h4>My Inbox</h4>
+            <div className="flex justify-between p-1 bg-neutral-300 border-b border-slate-400">
+              <h4>
+                My Inbox <span className="sr-only">Unread messages:</span>
+                <span
+                  className="inline-block w-6 mr-1 rounded-full bg-red-400 text-center"
+                  aria-label="unread messages"
+                >
+                  {myInbox.unreadChats.length}
+                </span>
+                <span className="sr-only">Recent chats:</span>
+                <span
+                  className="inline-block w-6 rounded-full bg-blue-300 text-center"
+                  aria-label="unread messages"
+                >
+                  {myInbox.recentChats.length}
+                </span>
+              </h4>
               <button
-                aria-label={inboxCollapsed ? "Show My Inbox" : "Hide My Inbox"}
-                onClick={() => setInboxCollapsed(!inboxCollapsed)}
+                aria-label={
+                  inboxTabsCollapsed.inboxCollapsed
+                    ? "Show My Inbox"
+                    : "Hide My Inbox"
+                }
+                onClick={() =>
+                  setInboxTabsCollapsed({
+                    ...inboxTabsCollapsed,
+                    inboxCollapsed: !inboxTabsCollapsed.inboxCollapsed,
+                  })
+                }
               >
-                {inboxCollapsed ? (
+                {inboxTabsCollapsed.inboxCollapsed ? (
                   <>
                     Show <ExpandMoreOutlined />
                   </>
@@ -103,52 +141,171 @@ const StaffChat = ({ setChatState, userDetails }: StaffChatProps) => {
                 )}
               </button>
             </div>
-            {!inboxCollapsed && (
-              <ol>
-                {myInbox.length ? (
-                  myInbox.map((inbox) => (
-                    <li
-                      className="p-1 bg-slate-100 hover:bg-slate-200 active:bg-slate-400 border-b border-slate-300"
-                      key={inbox.senderId}
-                    >
-                      <button
-                        className="flex flex-col w-full text-justify"
-                        onClick={() =>
-                          handleOpenChat(
-                            inbox.senderId,
-                            inbox.senderName,
-                            inbox.senderRole
-                          )
-                        }
+            {!inboxTabsCollapsed.inboxCollapsed && (
+              <>
+                <section>
+                  <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
+                    <h5>
+                      Unread Chats{" "}
+                      <span
+                        className="inline-block w-6 rounded-full bg-red-400 text-center"
+                        aria-label="unread messages"
                       >
-                        <div className="flex justify-between">
-                          <p>
-                            {inbox.senderName} - {inbox.senderRole}
-                          </p>
-                          <span
-                            className="w-6 rounded-full bg-red-400 text-center"
-                            aria-label="unread messages"
+                        {myInbox.unreadChats.length}
+                      </span>
+                    </h5>
+                    <button
+                      aria-label={
+                        inboxTabsCollapsed.unreadCollapsed
+                          ? "Show My Unread Chats"
+                          : "Hide My Unread Chats"
+                      }
+                      onClick={() =>
+                        setInboxTabsCollapsed({
+                          ...inboxTabsCollapsed,
+                          unreadCollapsed: !inboxTabsCollapsed.unreadCollapsed,
+                        })
+                      }
+                    >
+                      {inboxTabsCollapsed.unreadCollapsed ? (
+                        <>
+                          Show <ExpandMoreOutlined />
+                        </>
+                      ) : (
+                        <>
+                          Hide <ExpandLessOutlined />
+                        </>
+                      )}
+                    </button>
+                  </div>
+                  {!inboxTabsCollapsed.unreadCollapsed && (
+                    <ol>
+                      {myInbox.unreadChats.length ? (
+                        myInbox.unreadChats.map((unreadChat) => (
+                          <li
+                            className="p-1 bg-slate-100 hover:bg-slate-200 active:bg-slate-400 border-b border-slate-300"
+                            key={unreadChat.senderId}
                           >
-                            {inbox.unreadMessages}
-                          </span>
-                        </div>
-                        <div>
-                          <p>{truncatePreview(inbox.content)}</p>
-                          <time>{parseIsoToDateTime(inbox.mostRecent)}</time>
-                        </div>
-                      </button>
-                    </li>
-                  ))
-                ) : (
-                  <li className="p-1 bg-slate-100 border-b border-slate-300">
-                    <p>No new messages</p>
-                  </li>
-                )}
-              </ol>
+                            <button
+                              className="flex flex-col w-full text-justify"
+                              onClick={() =>
+                                handleOpenChat(
+                                  unreadChat.senderId,
+                                  unreadChat.senderName,
+                                  unreadChat.senderRole
+                                )
+                              }
+                            >
+                              <div className="flex justify-between">
+                                <p>
+                                  {unreadChat.senderName} -{" "}
+                                  {unreadChat.senderRole}
+                                </p>
+                                <span
+                                  className="w-6 rounded-full bg-red-400 text-center"
+                                  aria-label="unread messages"
+                                >
+                                  {unreadChat.unreadMessages}
+                                </span>
+                              </div>
+                              <div>
+                                <p>{truncatePreview(unreadChat.content)}</p>
+                                <time>
+                                  {parseIsoToDateTime(unreadChat.mostRecent)}
+                                </time>
+                              </div>
+                            </button>
+                          </li>
+                        ))
+                      ) : (
+                        <li className="p-1 bg-slate-100 border-b border-slate-300">
+                          <p>No new messages</p>
+                        </li>
+                      )}
+                    </ol>
+                  )}
+                </section>
+                <section>
+                  <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
+                    <h5>
+                      Recent Chats{" "}
+                      <span
+                        className="inline-block w-6 rounded-full bg-blue-300 text-center"
+                        aria-label="unread messages"
+                      >
+                        {myInbox.recentChats.length}
+                      </span>
+                    </h5>
+                    <button
+                      aria-label={
+                        inboxTabsCollapsed.recentCollapsed
+                          ? "Show My Recent Chats"
+                          : "Hide My Recent Chats"
+                      }
+                      onClick={() =>
+                        setInboxTabsCollapsed({
+                          ...inboxTabsCollapsed,
+                          recentCollapsed: !inboxTabsCollapsed.recentCollapsed,
+                        })
+                      }
+                    >
+                      {inboxTabsCollapsed.recentCollapsed ? (
+                        <>
+                          Show <ExpandMoreOutlined />
+                        </>
+                      ) : (
+                        <>
+                          Hide <ExpandLessOutlined />
+                        </>
+                      )}
+                    </button>
+                  </div>
+                  {!inboxTabsCollapsed.recentCollapsed && (
+                    <ol>
+                      {myInbox.recentChats.length ? (
+                        myInbox.recentChats.map((recentChat) => (
+                          <li
+                            className="p-1 bg-slate-100 hover:bg-slate-200 active:bg-slate-400 border-b border-slate-300"
+                            key={recentChat.senderId}
+                          >
+                            <button
+                              className="flex flex-col w-full text-justify"
+                              onClick={() =>
+                                handleOpenChat(
+                                  recentChat.senderId,
+                                  recentChat.senderName,
+                                  recentChat.senderRole
+                                )
+                              }
+                            >
+                              <div className="flex justify-between">
+                                <p>
+                                  {recentChat.senderName} -{" "}
+                                  {recentChat.senderRole}
+                                </p>
+                              </div>
+                              <div>
+                                <p>{truncatePreview(recentChat.content)}</p>
+                                <time>
+                                  {parseIsoToDateTime(recentChat.mostRecent)}
+                                </time>
+                              </div>
+                            </button>
+                          </li>
+                        ))
+                      ) : (
+                        <li className="p-1 bg-slate-100 border-b border-slate-300">
+                          <p>No recent chats</p>
+                        </li>
+                      )}
+                    </ol>
+                  )}
+                </section>
+              </>
             )}
           </section>
           <section>
-            <div className="flex justify-between p-1 bg-slate-200 border-b border-slate-400">
+            <div className="flex justify-between p-1 bg-neutral-300 border-b border-slate-400">
               <h4>My Staff</h4>
               <button
                 aria-label={
