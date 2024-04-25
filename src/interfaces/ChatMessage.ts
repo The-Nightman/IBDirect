179e4e60db74b3c644ffba5799947e2b31f6a379
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-25 15:32:18 +0100

    add ChatMessage interface

---
+++ src/interfaces/ChatMessage.ts
@@ -0,0 +1,12 @@
+export interface ChatMessage {
+  id: number;
+  content: string;
+  dateSent: string;
+  senderId: number;
+  senderName: string;
+  senderRole: string;
+  recipientId: number;
+  recipientName: string;
+  recipientRole: string;
+  read: boolean;
+}
