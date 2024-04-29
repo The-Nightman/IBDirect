6e6c1d545efece623bb8298051c6662eb06546d8
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-29 19:45:27 +0100

    fix styling breaking when encountering an error while loading patients upcoming appointments and surveys

--- src/components/Patients/PatientMyUpcomingAppSurvey.tsx
+++ src/components/Patients/PatientMyUpcomingAppSurvey.tsx
@@ -61,7 +61,7 @@ const PatientMyUpcomingAppSurvey = () => {
   return (
     <>
       {loading && <SpinnerStatus />}
-      <section className="flex flex-col lg:flex-row gap-8 p-4">
+      <section className="flex flex-col">
         {toastState.state && (
           <Toast
             color={toastState.color || "failure"}
@@ -69,58 +69,60 @@ const PatientMyUpcomingAppSurvey = () => {
             handleErrorState={closeToastState}
           />
         )}
-        <section className="lg:w-2/4">
-          <h3 className="border-b border-slate-400 mb-4">
-            My Upcoming Appointments
-          </h3>
-          <ol className="border-x border-t border-slate-500">
-            {upcomingData.appointments.length > 0 ? (
-              upcomingData.appointments.map((appointment, index) => {
-                return (
-                  <li key={index}>
-                    <PatientAppointmentCard
-                      appointment={appointment}
-                      index={index}
-                    />
-                  </li>
-                );
-              })
-            ) : (
-              <li>
-                <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
-                  <p>No upcoming appointments to display</p>
-                </div>
-              </li>
-            )}
-          </ol>
-        </section>
-        <section className="lg:w-2/4">
-          <h3 className="border-b border-slate-400 mb-4">
-            My Upcoming Surveys
-          </h3>
-          <ol className="border-x border-t border-slate-500">
-            {upcomingData.surveys.length > 0 ? (
-              upcomingData.surveys.map((survey, index) => {
-                return (
-                  <li key={index}>
-                    <PatientSurveyCard
-                      survey={survey}
-                      patientUserEdit={true}
-                      index={index}
-                      removeSurvey={removeCompletedSurvey}
-                    />
-                  </li>
-                );
-              })
-            ) : (
-              <li>
-                <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
-                  <p>No upcoming surveys to display</p>
-                </div>
-              </li>
-            )}
-          </ol>
-        </section>
+        <div className="flex lg:flex-row gap-8 p-4">
+          <section className="lg:w-2/4">
+            <h3 className="border-b border-slate-400 mb-4">
+              My Upcoming Appointments
+            </h3>
+            <ol className="border-x border-t border-slate-500">
+              {upcomingData.appointments.length > 0 ? (
+                upcomingData.appointments.map((appointment, index) => {
+                  return (
+                    <li key={index}>
+                      <PatientAppointmentCard
+                        appointment={appointment}
+                        index={index}
+                      />
+                    </li>
+                  );
+                })
+              ) : (
+                <li>
+                  <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
+                    <p>No upcoming appointments to display</p>
+                  </div>
+                </li>
+              )}
+            </ol>
+          </section>
+          <section className="lg:w-2/4">
+            <h3 className="border-b border-slate-400 mb-4">
+              My Upcoming Surveys
+            </h3>
+            <ol className="border-x border-t border-slate-500">
+              {upcomingData.surveys.length > 0 ? (
+                upcomingData.surveys.map((survey, index) => {
+                  return (
+                    <li key={index}>
+                      <PatientSurveyCard
+                        survey={survey}
+                        patientUserEdit={true}
+                        index={index}
+                        removeSurvey={removeCompletedSurvey}
+                      />
+                    </li>
+                  );
+                })
+              ) : (
+                <li>
+                  <div className="w-full p-1 border-b border-slate-600 bg-slate-200">
+                    <p>No upcoming surveys to display</p>
+                  </div>
+                </li>
+              )}
+            </ol>
+          </section>
+        </div>
       </section>
     </>
   );
