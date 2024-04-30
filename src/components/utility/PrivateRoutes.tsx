893e5d10cee3b3498f05046e874af10cc8910652
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-30 15:17:14 +0100

    improve authContext and PrivateRoutes to fix auth check failing on page refresh or url navigation

--- src/components/utility/PrivateRoutes.tsx
+++ src/components/utility/PrivateRoutes.tsx
@@ -1,15 +1,24 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import SpinnerStatus from "../UX/Spinner";
 
 const PrivateRoutes = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const jwt = sessionStorage.getItem("jwt");
   let auth = false;
   if (user.userID && jwt) {
     auth = true;
-  } else {
-    auth = false;
   }
-  return <>{auth ? <Outlet /> : <Navigate to={"/"}/>}</>;
+  if (loading) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <p className="animate-pulse absolute top-[45%] z-[100] text-2xl text-white">
+          Checking User Authentication
+        </p>
+        <SpinnerStatus color="success" />
+      </div>
+    );
+  }
+  return <>{user && jwt ? <Outlet /> : <Navigate to={"/"} />}</>;
 };
 export default PrivateRoutes;
